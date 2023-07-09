b231851449d78aed4c7f86a20d7a57247cf26ecb
alien <aliedevuz@gmail.com>
2023-07-09 22:01:55 +0500

    Saytga biroz o'zgartirishlar kiritildi va ma'lumotlar bilan boyitildi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let navbar = document.querySelector('.header .navbar')
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
 
 document.querySelector('#menu-btn').onclick = () =>{
   navbar.classList.add('active');
@@ -8,32 +9,19 @@ document.querySelector('#close-navbar').onclick = () =>{
   navbar.classList.remove('active');
 };
 
-let registerBtn = document.querySelector('.account-form .register-btn');
-let loginBtn = document.querySelector('.account-form .login-btn');
-
-registerBtn.onclick = () =>{
-  registerBtn.classList.add('active');
-  loginBtn.classList.remove('active');
-  document.querySelector('.account-form .login-form').classList.remove('active');
-  document.querySelector('.account-form .register-form').classList.add('active');
+document.querySelector('.load-more .btn').onclick = () =>{
+  document.querySelectorAll('.courses .box-container .hide').forEach(show =>{
+    show.style.display = 'block';
+  });
+  document.querySelector('.load-more .btn').style.display = 'none';
 };
 
-loginBtn.onclick = () =>{
-  registerBtn.classList.remove('active');
-  loginBtn.classList.add('active');
-  document.querySelector('.account-form .login-form').classList.add('active');
-  document.querySelector('.account-form .register-form').classList.remove('active');
-};
-
-let accountForm = document.querySelector('.account-form')
-
-document.querySelector('#account-btn').onclick = () =>{
-  accountForm.classList.add('active');
-}
-
-document.querySelector('#close-form').onclick = () =>{
-  accountForm.classList.remove('active');
-};
+accordion.forEach(acco =>{
+  acco.onclick = () =>{
+    accordion.forEach(dion => dion.classList.remove('active'));
+    acco.classList.toggle('active');
+  };
+});
 
 var swiper = new Swiper(".home-slider", {
   pagination: {
@@ -117,19 +105,3 @@ var swiper = new Swiper(".logo-slider", {
     },
   },
 });
-
-let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
-
-accordion.forEach(acco =>{
-  acco.onclick = () =>{
-    accordion.forEach(dion => dion.classList.remove('active'));
-    acco.classList.toggle('active');
-  };
-});
-
-document.querySelector('.load-more .btn').onclick = () =>{
-  document.querySelectorAll('.courses .box-container .hide').forEach(show =>{
-    show.style.display = 'block';
-  });
-  document.querySelector('.load-more .btn').style.display = 'none';
-};
